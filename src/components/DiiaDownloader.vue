9119d603ec948544748072b5beb18f62593941c1
Amice13 <kirillzakharov13@gmail.com>
2025-09-28 14:48:51 +0000

    Minor updates

--- src/components/DiiaDownloader.vue
+++ src/components/DiiaDownloader.vue
@@ -3,25 +3,31 @@
     <v-row>
       <v-col>
         <div>
-          <div class="d-flex align-center justify-center">
-            <v-img
-              class="mt-4"
-              height="150"
-              width="150"
-              max-width="150"
-              src="/diia.svg"
-            />
-            <div class="ml-4 text-h1 font-weight-black">злив</div>
+          <div class="text-center">
+            <div class="d-inline-flex align-center justify-center bg-black margin-auto rounded-xl">
+              <v-img
+                class="mt-4"
+                height="150"
+                width="150"
+                max-width="150"
+                src="/diia.svg"
+              />
+              <div class="mt-2 text-h2 font-weight-black pr-8">злив</div>
+            </div>
           </div>
           <div class="mb-8 mt-8 text-center">
-            <div class="text-h4 font-weight-light mb-n1">Завантаження свіжих дампів із "Дії"</div>
+            <div class="text-h4 font-weight-light mb-n1">Перший недержавний сервіс<br/>з отримання завжди свіжих зливів із "Дії"</div>
+            <div class="mt-2">Перед завантаженням або обуренням ознайомтеся із <v-chip @click="dialog = true" color="red-darken-2">політикою сервісу</v-chip></div>
           </div>
 
-          <div class="mb-8 text-center">
-            <div class="text-h6 font-weight-light mb-n1">Визначіть скільки рядків даних ви хочете завантажити</div>
+          <div class="mb-6 text-center">
+            <div class="text-h6 font-weight-light mb-n1">Визначіть, скільки рядків даних ви хочете завантажити</div>
             <div class="mt-4 d-flex align-center justify-center w-100">
               <v-text-field
                 v-model="rows"
+                @blur="rows = parseInt(String(rows))"
+                type="number"
+                min="1"
                 variant="outlined"
                 max-width="300"
                 class="text-center bg-grey-lighten-3 centered-input"
@@ -43,6 +49,27 @@
         </div>
       </v-col>
     </v-row>
+    <v-dialog v-model="dialog" max-width="680">
+      <v-card>
+        <v-card-title>
+          Політика сервісу
+        </v-card-title>
+        <v-card-text>
+          <ul class="ml-8 mr-4 text-justify">
+            <li class="mb-4">"<a target="_blank" href="https://www.facebook.com/mintsyfra/posts/1198099892359325">Дія не зберігає персональних даних</a> — система працює за принципом data-in-transit: інформація підтягується з державних реєстрів у момент запиту і не накопичується в застосунку чи на порталі".</li>
+            <li class="mb-4">Даний сервіс є генератором випадкових записів, які зберігаються за структурою, яка відповідає наявним "зливам" додатку Дія, розповсюдженим у мережі (включаючи заголовки російською мовою).</li>
+            <li class="mb-4">Дані генеруються на стороні користувача, жодних запитів до третіх сторін не здійснюється, жодні дані користувачів не зберігаються</li>
+            <li class="mb-4">Для генерації випадкових записів використані набори відкритих даних Укрпошти (<a target="_blank" href="https://www.ukrposhta.ua/files/shares/out/houses.zip">"Довідник поштових адрес"</a>), Державної міграційної служби України (<a target="_blank" href="https://dmsu.gov.ua/assets/files/doc/n_2017_327_dod_2.xls">Довідник кодів та назв структурних підрозділів апарату, територіальних органів та територіальних підрозділів Державної міграційної служби України</a>), Міністерства внутрішніх справ України (<a target="_blank" href="https://zakon.rada.gov.ua/laws/show/730-2015-%D0%BF#Text">"Перелік територіальних органів Міністерства внутрішніх справ, що ліквідуються"</a>). Усі інші дані генеруються за допомогою бібліотеки faker або алгоритмів для генерації ідентифікаторів фізичних осіб та організацій у відповідності до чинного законодавства.</li>
+            <li class="mb-4">Дані генеруються таким чином, щоб успішно проходити основні логічні перевірки (контрольні суми РНОКПП, кодів ЄДРПОУ, вік, стать особи, інформацію про видачу документів, тощо). При великому експорті даних не гарантується унікальність ідентифікаторів. Масив, що генерується, включатиме осіб від 14 до 100 років, проте кінцевий розподіл не відповідатиме фактичному демографічному розподілу. Дані про місце отримання документів та місце проживання особи можуть містити дані про різні населені пункти.</li>
+            <li class="mb-4">Завантажені дані можуть бути вільно розповсюджені без жодних обмежень. Відповідальність за подальшу обробку даних зберігається за користувачами. Бажано не підпадати під ст. 190 ККУ.</li>
+            <li>Безпеки вам та вашим персональним даним!</li>
+          </ul>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn @click="dialog = false" color="black" variant="tonal">Я зрозумів</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -68,6 +95,7 @@ function formatBytes(bytes: number, decimals = 1): string {
   return `${value.toFixed(decimals)} ${sizes[i]}`
 }
 
+const dialog = ref(false)
 const rows = ref(1000)
 
 if ('serviceWorker' in navigator) {
@@ -76,7 +104,8 @@ if ('serviceWorker' in navigator) {
 const csvGenerator = new csvWorker()
 
 const saveData = async () => {
-  const fileStream = streamSaver.createWriteStream('DIIA_users_DB_2025.csv')
+  const currentYear = new Date().getFullYear()
+  const fileStream = streamSaver.createWriteStream(`DIIA_users_DB_${String(currentYear)}.csv`)
   const writer = fileStream.getWriter()
 
   csvGenerator.onmessage = async e => {
